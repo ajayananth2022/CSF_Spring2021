43ccf2c49e13cae5ad0fe14b5bee9d5bd64221c7
LucyZhang <wzhang99@jhu.edu>
2021-03-10 12:34:27 -0500

    changed files to .cpp and changed Makefile

--- csf_assign03/csim.cpp
+++ csf_assign03/csim.cpp
@@ -1,5 +1,5 @@
  //Lucy Zhang, Ajay Ananthakrishnan
  //wzhang99@jhu.edu, ajayananth@jhu.edu
 
-#include <stdio.h>
+#include <iostream>
 #include "csim.h"
--- csf_assign03/main.cpp
+++ csf_assign03/main.cpp
@@ -1,6 +1,6 @@
  //Lucy Zhang, Ajay Ananthakrishnan
  //wzhang99@jhu.edu, ajayananth@jhu.edu
-#include <stdio.h>
+#include <iostream>
 #include "csim.h"
 
 
