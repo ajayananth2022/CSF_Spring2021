340e35dd440f0f1bc78207a890e8ae19524b762f
Wenyan Zhang <wenyanzhang@Wenyans-MacBook-Air.local>
2021-03-23 19:52:38 -0400

    added helper functions

--- csf_assign03/main.cpp
+++ csf_assign03/main.cpp
@@ -14,6 +14,31 @@ int main(int argc, char **argv) {
 	    cout << "Invalid number of arguments!" << endl;
 	    return 1;
     }
+
+    if (atoi(argv[1]) <= 0 || !checkPowerTwo(atoi(argv[1]))) {
+	    cout << "Invalid number of sets in the cache!" << endl;
+	    return 1;
+    }
+    if (atoi(argv[2]) <= 0 || !checkPowerTwo(atoi(argv[2]))) {
+	    cout << "Invalid number of blocks in the cache!" << endl;
+        return 1;
+    }
+    if (atoi(argv[3]) < 4 || !checkPowerTwo(atoi(argv[3]))) {
+	    cout << "Invalid size of blocks in the cache!" << endl;
+	    return 1;
+    }
+    if (argv[4] != "write-allocate" && argv[4] != "no-write-allocate") {
+        cout << "Invalid write-miss strategy!" << endl;
+	    return 1;
+    }
+    if (argv[5] != "write-through" && argv[5] != "write-back") {
+        cout << "Invalid write-hit strategy!" << endl;
+	    return 1;
+    }
+    if (argv[6] != "lru" && argv[6] != "fifo") {
+        cout << "Invalid replace strategy!" << endl;
+	    return 1;
+    }
     Simulator sim(argv);
     string mem_access;
     while (getline(cin, mem_access)) {
