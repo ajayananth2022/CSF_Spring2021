a876af57605f7aca39de9693e46293695a45f6c2
Wenyan Zhang <wenyanzhang@Wenyans-MacBook-Air.local>
2021-04-30 00:13:55 -0400

    made comments consistent

--- csf_assign06/calcServer.c
+++ csf_assign06/calcServer.c
@@ -36,34 +36,32 @@ int chat_with_client(struct Calc *calc, int infd, int outfd) {
 	rio_t in;
 	char linebuf[LINEBUF_SIZE];
 
-	/* wrap standard input (which is file descriptor 0) */
+	// wrap standard input (which is file descriptor 0)
 	rio_readinitb(&in, infd);
 
-	/*
-	 * Read lines of input, evaluate them as calculator expressions,
-	 * and (if evaluation was successful) print the result of each
-	 * expression.  Quit when "quit" command is received.
-	 */
+	 // Read lines of input, evaluate them as calculator expressions,
+	 // and (if evaluation was successful) print the result of each
+	 // expression.  Quit when "quit" command is received.
 	int status = 0;
 	while (!status) {
 		ssize_t n = rio_readlineb(&in, linebuf, LINEBUF_SIZE);
 		if (n <= 0) {
-			/* error or end of input */
+			//error or end of input 
 			status = 1;
 		} else if (strcmp(linebuf, "quit\n") == 0 || strcmp(linebuf, "quit\r\n") == 0) {
-			/* quit command */
+			//quit command 
 			status = 1;
 		} else if (strcmp(linebuf, "shutdown\n") == 0 || strcmp(linebuf, "shutdown\r\n") == 0) {
-			/* shutdown command */
+			//shutdown command 
 			status = 2;
 		} else {
-			/* process input line */
+			//process input line 
 			int result;
 			if (calc_eval(calc, linebuf, &result) == 0) {
-				/* expression couldn't be evaluated */
+				// expression couldn't be evaluated 
 				rio_writen(outfd, "Error\n", 6);
 			} else {
-				/* output result */
+				// output result
 				int len = snprintf(linebuf, LINEBUF_SIZE, "%d\n", result);
 				if (len < LINEBUF_SIZE) {
 					rio_writen(outfd, linebuf, len);
